9ad442547bc85061a9e69be77c9ed5ef8d08ac05
joacomoreno-droid <joaquinmorenotdf@gmail.com>
2026-03-12 19:33:09 -0300

    Update exercise_price.py

--- exercise_price.py
+++ exercise_price.py
@@ -9,3 +9,13 @@ def price():
     4. El precio final (subtotal + propina)
     """
     precio_base = 100
+
+    impuesto = precio_base * 0.21
+    subtotal = precio_base + monto_impuesto
+    propina = subtotal * 0.10
+    precio_final = subtotal + propina
+    
+    print(f"El monto del impuesto: {impuesto}")
+    print(f"El subtotal: {subtotal}")
+    print(f"El monto de la propina: {propina}") 
+    print(f"El precio final: {precio_final}")
