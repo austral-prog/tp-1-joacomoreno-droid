9d79e2c433cdcaf585eb67a2331aea319f9ccc81
joacomoreno-droid <joaquinmorenotdf@gmail.com>
2026-03-17 17:39:29 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -12,6 +12,11 @@ def grades():
     nota2 = 7
     nota3 = 9
 
+    promedio = (nota1 + nota2 + nota3) / 3
+    nota_max =  max(nota1, nota2, nota3)
+    nota_min = min(nota1, nota2, nota3)
+    puntos_faltantes = 10 - promedio
+
     print(promedio)
     print(nota_max)
     print(nota_min)
